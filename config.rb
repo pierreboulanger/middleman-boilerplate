57d26262d85305e43d5ed877195f24fa5c7ca12b
Pierre Boulanger <pierre.boulanger1@gmail.com>
2016-08-29 15:19:52 +0200

    done

--- config.rb
+++ config.rb
@@ -11,6 +11,10 @@
 # Page options, layouts, aliases and proxies
 ###
 
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 # Per-page layout changes:
 #
 # With no layout
